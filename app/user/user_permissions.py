7db726793f447f0bf0be65f128a8dbca89d6909a
baiemir <baemir.omurzak0v@mail.ru>
2025-11-30 13:50:12 +0600

    create file user_permissions

---
+++ app/user/user_permissions.py
@@ -0,0 +1,10 @@
+from fastapi import Depends, HTTPException
+from app.user.models.user import UserRole
+from app.user.services import fastapi_users
+
+
+def admin_only(user=Depends(fastapi_users.current_user())):
+    if user.role != UserRole.ADMIN:
+        raise HTTPException(status_code=403, detail="Admins only")
+    return user
+
