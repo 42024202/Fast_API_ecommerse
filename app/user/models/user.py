29d160e6843bac0eb4400b92640da34ec46f2fb0
baiemir <baemir.omurzak0v@mail.ru>
2025-11-30 13:35:22 +0600

    User roles was added

--- app/user/models/user.py
+++ app/user/models/user.py
@@ -1,17 +1,32 @@
+import enum
+
 from typing import TYPE_CHECKING
 from app.core import Base
 from app.mixins.id_pk_mixin import IdPkMixin
-from fastapi import Depends
 from fastapi_users.db import SQLAlchemyBaseUserTable, SQLAlchemyUserDatabase
+
+from sqlalchemy import Enum as PgEnum
 from sqlalchemy.orm import Mapped, mapped_column
 
 
 if TYPE_CHECKING:
     from sqlalchemy.ext.asyncio import AsyncSession
 
+
+class UserRole(str, enum.Enum):
+    USER = "user",
+    ADMIN = "admin"
+
+
 class User(IdPkMixin, SQLAlchemyBaseUserTable, Base):
     __tablename__ = 'user'
     
+    role:Mapped[UserRole] = mapped_column(
+            PgEnum(UserRole, name="userrole"),
+            default=UserRole.USER,
+            nullable=False
+            )
+
     @classmethod
     def get_db(cls, session: "AsyncSession"):
         return SQLAlchemyUserDatabase(session, User)
